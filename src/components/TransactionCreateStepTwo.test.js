a2fb003a7ab8c6f35090915095410a4cf0dbd310
Muhodari <sagemuho@gmail.com>
2021-10-29 09:18:19 +0200

    finished unit test with conditioal rendering

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,4 +1,5 @@
 import { render ,screen} from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
 
 test('on initial render, the pay button is disabled ', async ()=>{
@@ -7,4 +8,15 @@ test('on initial render, the pay button is disabled ', async ()=>{
   // screen.debug()
 
   expect(await screen.findByRole('button',{name:/pay/i})).toBeDisabled();
+})
+
+
+test('if an amount and note is entered, pay button becomes enabled ', async ()=>{
+  render(<TransactionCreateStepTwo sender={{id:'5'}} receiver={{id:'5'}}/>)
+ userEvent.type(screen.getByPlaceholderText(/amount/i),"50");
+ userEvent.type(screen.getByPlaceholderText(/add a note/i),"dinner");
+
+//  screen.getByRole('')
+expect(await screen.findByRole('button',{name:/pay/i})).toBeEnabled();
+
 })
