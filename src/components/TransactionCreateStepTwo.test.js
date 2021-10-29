61a55e5c959db20f258ad0eaca25508a491d1754
Muhodari <sagemuho@gmail.com>
2021-10-29 09:26:08 +0200

    done with integration test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -2,11 +2,11 @@ import { render ,screen} from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
 
-test('on initial render, the pay button is disabled ', async ()=>{
 
+//        UNIT TEST         //
+test('on initial render, the pay button is disabled ', async ()=>{
   render(<TransactionCreateStepTwo sender={{id:'5'}} receiver={{id:'5'}}/>)
   // screen.debug()
-
   expect(await screen.findByRole('button',{name:/pay/i})).toBeDisabled();
 })
 
@@ -15,8 +15,25 @@ test('if an amount and note is entered, pay button becomes enabled ', async ()=>
   render(<TransactionCreateStepTwo sender={{id:'5'}} receiver={{id:'5'}}/>)
  userEvent.type(screen.getByPlaceholderText(/amount/i),"50");
  userEvent.type(screen.getByPlaceholderText(/add a note/i),"dinner");
-
 //  screen.getByRole('')
 expect(await screen.findByRole('button',{name:/pay/i})).toBeEnabled();
 
 })
+
+
+//      INTEGRATION TEST COMBINNING THE TWO ABOVE TESTS                  //
+
+
+test('on initial render, the pay button is disabled AND\nif an amount and note is entered, pay button becomes enabled ', async ()=>{
+  render(<TransactionCreateStepTwo sender={{id:'5'}} receiver={{id:'5'}}/>)
+
+  expect(await screen.findByRole('button',{name:/pay/i})).toBeDisabled();
+ userEvent.type(screen.getByPlaceholderText(/amount/i),"50");
+ userEvent.type(screen.getByPlaceholderText(/add a note/i),"dinner");
+
+expect(await screen.findByRole('button',{name:/pay/i})).toBeEnabled();
+
+})
+
+
+
