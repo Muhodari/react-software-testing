f2ffb3c40b2884a12308a3b7828c33a919cdcb99
Muhodari <sagemuho@gmail.com>
2021-11-01 20:47:56 +0200

    outlined the content

---
+++ cypress/integration/payment_spec.js
@@ -0,0 +1,17 @@
+describe('payment',()=>{
+ 
+it('user can make payment',()=>{
+// login
+// check account balance 
+// click on pay button
+//search for the user
+// add amount and note  and click pay
+// return to transactions
+// go to personal payment
+//click on payment
+// vertify if the payment was made
+// verify if the payment amount was deducted 
+ 
+ })
+
+});
